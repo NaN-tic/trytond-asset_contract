5093fda688c5d976ecfa1fc08d326fb663ba3377
Albert Cervera i Areny <albert@nan-tic.com>
2016-04-29 10:26:20 +0200

    Ensure the same asset is not used in two different contracts in overlapping time periods.

--- contract.py
+++ contract.py
@@ -1,25 +1,28 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import fields
-from trytond.pool import PoolMeta
+import datetime
+from sql.conditionals import Coalesce
+from trytond.model import ModelView, Workflow, fields
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
 
-__all__ = ['Asset', 'ContractLine', 'ContractConsumption']
+__all__ = ['Contract', 'ContractLine', 'ContractConsumption']
 
 
-class Asset:
-    __name__ = 'asset'
+class Contract:
+    __name__ = 'contract'
     __metaclass__ = PoolMeta
-    contract_lines = fields.One2Many('contract.line', 'asset',
-        'Contract Lines')
 
     @classmethod
-    def copy(cls, assets, default=None):
-        if default is None:
-            default = {}
-        default = default.copy()
-        if not 'contract_lines' in default:
-            default['contract_lines'] = None
-        return super(Asset, cls).copy(assets, default)
+    @ModelView.button
+    @Workflow.transition('confirmed')
+    def confirm(cls, contracts):
+        super(Contract, cls).confirm(contracts)
+        # Check dates after confirmation as check_dates() ensures the contract
+        # is in 'confirmed' state.
+        for contract in contracts:
+            for line in contract.lines:
+                line.check_dates()
 
 
 class ContractLine:
@@ -29,10 +32,57 @@ class ContractLine:
         'on_change_with_asset_party')
     asset = fields.Many2One('asset', 'Asset')
 
+    @classmethod
+    def __setup__(cls):
+        super(ContractLine, cls).__setup__()
+        cls._error_messages.update({
+                'asset_has_contract': ('Contract line "%(contract_line)s" '
+                    'is not valid because asset "%(asset)s" is already '
+                    'assigned to contract line "%(overlapping_line)s".')
+                })
+
     @fields.depends('contract')
     def on_change_with_asset_party(self, name=None):
         if self.contract and self.contract.party:
             return self.contract.party.id
+
+    @classmethod
+    def validate(cls, lines):
+        for line in lines:
+            line.check_dates()
+
+    def check_dates(self):
+        if not self.asset:
+            return
+        if not self.contract.state in ('confirmed', 'finished'):
+            return
+        Contract = Pool().get('contract')
+        cursor = Transaction().cursor
+        max_date = datetime.date(datetime.MAXYEAR, 12, 31)
+        start_date = self.start_date
+        end_date = self.end_date or max_date
+        table = self.__table__()
+        contract = Contract.__table__()
+        cursor.execute(*table.join(contract, condition=(table.contract ==
+                    contract.id)).select(table.id, where=((
+                        (table.start_date <= start_date)
+                        & (Coalesce(table.end_date, max_date) >= start_date))
+                    | ((table.start_date <= end_date)
+                        & (Coalesce(table.end_date, max_date) >= end_date))
+                    | ((table.start_date >= start_date)
+                        & (Coalesce(table.end_date, max_date) <= end_date)))
+                & (contract.state.in_(['confirmed', 'finished']))
+                & (table.asset == self.asset.id)
+                & (table.id != self.id),
+                limit=1))
+        overlapping_record = cursor.fetchone()
+        if overlapping_record:
+            overlapping_line = self.__class__(overlapping_record[0])
+            self.raise_user_error('asset_has_contract', {
+                    'contract_line': self.rec_name,
+                    'asset': self.asset.rec_name,
+                    'overlapping_line': overlapping_line.rec_name,
+                    })
 
 
 class ContractConsumption:
