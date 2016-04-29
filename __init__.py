f1dd8ce72e7109bd6897499458dec45cdb48ca5c
Albert Cervera i Areny <albert@nan-tic.com>
2016-04-29 10:26:20 +0200

    Ensure the same asset is not used in two different contracts in overlapping time periods. (grafted from 2d4d1df77a98ea6f4203d4faf14e876e9f04c40a)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from .asset import *
 from .contract import *
 
 
 def register():
     Pool.register(
-        ContractLine,
         Asset,
+        Contract,
+        ContractLine,
         ContractConsumption,
         module='asset_contract', type_='model')
---
+++ asset.py
@@ -0,0 +1,24 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['Asset']
+
+
+class Asset:
+    __name__ = 'asset'
+    __metaclass__ = PoolMeta
+    contract_line = fields.Function(fields.Many2One('contract.line',
+            'Current Contract Line'), 'get_contract_line')
+    contract_lines = fields.One2Many('contract.line', 'asset',
+        'Contract Lines', readonly=True)
+
+    @classmethod
+    def copy(cls, assets, default=None):
+        if default is None:
+            default = {}
+        default = default.copy()
+        if not 'contract_lines' in default:
+            default['contract_lines'] = None
+        return super(Asset, cls).copy(assets, default)
